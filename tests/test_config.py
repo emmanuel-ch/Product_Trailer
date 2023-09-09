bb94659e6d02e800d81e0bd0a39dbbfded64f837
emmanuel-ch <emmanuel.chenailler@gmail.com>
2023-09-09 11:26:14 +0200

    Add tests on profile creation and recovery

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,8 +2,10 @@
 Tests on validate_configname, Config class.
 """
 
+from pathlib import Path
+import shutil
 import pytest
-from product_trailer.config import validate_configname
+from product_trailer.config import validate_configname, Config
 
 
 
@@ -14,3 +16,47 @@ from product_trailer.config import validate_configname
 def test_validate_configname(test_input, expected):
     assert validate_configname(test_input) == expected
 
+
+class TestConfigCreation():
+    
+    def test_create_profile_directory(self):
+        config_name = 'a_test_config_for_testing'
+        _ = Config(config_name)
+
+        profile_path = Path('profiles') / config_name
+        profile_exists = profile_path.is_dir()
+
+        if profile_exists:
+            shutil.rmtree(profile_path)
+
+        assert profile_exists
+    
+
+    def test_recover_profile_no_database(self):
+        config_name = 'a_test_config_for_testing'
+        _ = Config(config_name)
+        profile_db_path = Path('profiles') / config_name / 'database'
+        shutil.rmtree(profile_db_path)
+
+        _ = Config(config_name)
+        db_exists = profile_db_path.is_dir()
+
+        shutil.rmtree((Path('profiles') / config_name))
+
+        assert db_exists
+    
+
+    def test_recover_profile_no_config(self):
+        config_name = 'a_test_config_for_testing'
+        _ = Config(config_name)
+        profile_config_path = Path('profiles') / config_name / 'config'
+        shutil.rmtree(profile_config_path)
+
+        _ = Config(config_name)
+        db_exists = profile_config_path.is_dir()
+
+        shutil.rmtree((Path('profiles') / config_name))
+
+        assert db_exists
+    
+    
